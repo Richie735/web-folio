d2d724875fd741f83e72a7e55b0875bba7b210ad
Richie735 <richierichie735@gmail.com>
2023-02-13 15:44:39 +0000

    Experience/Education Timeline

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
 export { default as About } from "./about/About";
-export { default as Experience } from "./Experience";
+export { default as Experience } from "./experience/Experience";
 export { default as Header } from "./header/Header";
 export { default as Hero } from "./hero/Hero.jsx";
 export { default as Projects } from "./projects/Projects";
